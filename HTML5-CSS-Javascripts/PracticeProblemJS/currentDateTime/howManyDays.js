b2eadae5075e1b40674ca9e68405b740fdf765a0
bonnratha <40635260+bonnratha@users.noreply.github.com>
2018-07-02 15:49:25 -0400

    Edit

--- HTML5-CSS-Javascripts/PracticeProblemJS/currentDateTime/howManyDays.js
+++ HTML5-CSS-Javascripts/PracticeProblemJS/currentDateTime/howManyDays.js
@@ -1,4 +1,4 @@
-const testday = new Date("March 1, 2012");
+const testday = new Date("April 1, 2012");
 
 
 // Declare some variables and values
@@ -9,13 +9,14 @@ console.log(tDate+" "+ tMonth+" "+tYear);
 
 
 var monthLength = [31,28,31,30,31,30,31,31,30,31,30,31];
-if(tYear%4==0) {monthLength[1] = 29;}
+if(tYear%4==0) {monthLength[1] = 29; console.log("YESSSS");}
+console.log(tMonth);
 
 function calculateTotalDays(months)
 {
   let i = 0;
   var dayss = 0;
-  while (i<months-1){
+  while (i<months){
     dayss += monthLength[i];
     i++; }
   console.log(dayss);
