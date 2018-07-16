4f384ff4fde485373e1ae18572083f8bd90fd7ac
bonnratha <40635260+bonnratha@users.noreply.github.com>
2018-07-16 15:29:45 -0400

    Stuffs

--- justATest.js
+++ justATest.js
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+
+var test = 'The doctor said "Today is your lucky day!" ';
+var test1 = "I'm going to be happy when you give me the news!";
+var test3 = 'The doctor said "I\'m pleased to announce that it\'s a girl!" ';
+var test4 = "The doctor said \"I'm pleased to announce that it's a girl!\" ";
+// console.log(test4);
+
+
+var test5 = 'Hello';
+console.log(isNaN(test5));
